c1cb00de57d4cf8c6ef666ceb405917deb0fe965
Adam1TBC <Twilightsparkeladam@gmail.com>
2016-04-29 20:52:14 +0300

    fdsf

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    let ArrayOfLessons = ["Hello world"]
+    let ArrayOfLessons = ["Hello world" , "Hello world 2"]
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
